1e0b2dbbdafdf50710d30d4c32169af6631d0ee0
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-08-10 00:00:16 +0530

    updated

---
+++ JS project/1. Dice game/index.js
@@ -0,0 +1,16 @@
+//dice 1
+var randomNumber1=Math.floor(Math.random()*6)+1;
+console.log(randomNumber1);
+var randomImage1= "./images/dice"+randomNumber1+".png";
+var image1=document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
+
+//dice 2
+var randomNumber2=Math.floor(Math.random()*6)+1;
+console.log(randomNumber2);
+var randomImage2 ="./images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1].setAttribute("src",randomImage2)
+
+//winner
+if(randomNumber1>randomNumber2){
+    document.getElementsByClassName("p")
+}
